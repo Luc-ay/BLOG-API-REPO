337ce84126ab28adf0196a162527ee4150a147a9
Ayomide <ayomideayomipo147@gmail.com>
2025-01-12 01:30:08 +0100

    Update registerController file

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -51,3 +51,24 @@ export const registerController = async (req, res) => {
     res.status(500).json({ error: error.message })
   }
 }
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      return res.status(400).json({ Msg: 'User does not exist' })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      return res.status(400).json({ Msg: 'Invalid credentials' })
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    user.password = undefined
+    res.status(200).json({ token, user })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import upload from '../middlewares/picture.storage.middleware.js'
-import { registerController } from '../controllers/register.controller.js'
+import {
+  loginController,
+  registerController,
+} from '../controllers/register.controller.js'
 
 const router = express.Router()
 
 router.post('/register', upload.single('picture'), registerController)
+router.post('/login', loginController)
 
 export default router
